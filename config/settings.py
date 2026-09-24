154a8ef0f0b221e3b42f0ed99e53fead3c20dc83
anujeditsbyanuj-bit <anujeditsbyanuj@gmail.com>
2026-09-24 12:47:31 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -3,17 +3,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-TELEGRAM_API_ID = os.getenv('TELEGRAM_API_ID')
-TELEGRAM_API_HASH = os.getenv('TELEGRAM_API_HASH')
-DISKWALA_API_KEY = os.getenv('DISKWALA_API_KEY', '')
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN',"8884894646:AAFtQFJnhsoHUZgiANYzrJkur6v9qeMe3CA")
+TELEGRAM_API_ID = os.getenv('TELEGRAM_API_ID', "33029767")
+TELEGRAM_API_HASH = os.getenv('TELEGRAM_API_HASH', "5d897bed11bc8b062a12f6c1c3c5360a")
+DISKWALA_API_KEY = os.getenv('DISKWALA_API_KEY', '693e34245ea7ab1dfade3755')
 
 DOWNLOAD_DIR = os.getenv('DOWNLOAD_DIR', './downloads')
 MAX_FILE_SIZE = int(os.getenv('MAX_FILE_SIZE', 2000 * 1024 * 1024))
 
 LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')
 
-ALLOWED_USERS_STR = os.getenv('ALLOWED_USERS', '').strip()
+ALLOWED_USERS_STR = os.getenv('ALLOWED_USERS', '8931907813').strip()
 ALLOWED_USERS = [uid.strip() for uid in ALLOWED_USERS_STR.split(',') if uid.strip()] if ALLOWED_USERS_STR else []
 
 BOT_ADMIN_IDS_STR = os.getenv('BOT_ADMIN_IDS', '').strip()
